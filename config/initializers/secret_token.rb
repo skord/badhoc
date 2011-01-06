295de108d5300ec52148d5a194bb56ab1f68469f
Mike Danko <mike@l4m3.com>
2011-01-06 15:59:36 -0500

    initial commit with formtastic, rspec, and flutie ready to go

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Badhoc::Application.config.secret_token = 'f560ce17081429960ca9267e07dda2a00dcd776177d6989857c58d502811c53d9017c0344a35d671bcd10e370af833276059b1a20c8ff67ca0fd0f0e24ffcf3d'
