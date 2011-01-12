260f6c028667fac042706225bdc1a3c6068a024e
Mike Danko <mike@l4m3.com>
2011-01-12 11:23:28 -0500

    trying iphone views on

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :adjust_format_for_iphone
+  
+  private
+  def adjust_format_for_iphone
+    request.format = :iphone if iphone_request?
+  end
+  
+  def iphone_request?
+    return (request.subdomains.first == "iphone" || params[:format] == "iphone")
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def iphone_user_agent?
+    request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(Mobile\/.+Safari)/]
+  end
 end
