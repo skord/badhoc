d8c82d6a1df57927871524cd324b6f650ddc436a
Mike Danko <mike@l4m3.com>
2011-01-14 17:03:41 -0500

    static asset management to be done by passenger standalone for the time being

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,5 +23,6 @@ Badhoc::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  config.serve_static_assets = false
 end
 
