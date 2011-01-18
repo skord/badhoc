517b3c20f09446b3564c94c9feb00eb7ff36620d
Mike Danko <mike@l4m3.com>
2011-01-18 14:48:41 -0500

    removed terrible helper, fixes #8839969 #6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,14 +4,6 @@ module ApplicationHelper
     request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(Mobile\/.+Safari)/]
   end
   
-  def tripcode_auth_tags(tripcode_status, name)
-    if tripcode_status
-      "<strong class='authenticated'>#{name}</strong>"
-    else
-      "<strong>#{name}</strong>"
-    end
-  end
-  
   def user_text_markdown(text, truncate_to = nil)
     if truncate_to == nil
       sanitize RDiscount.new(text, :filter_html, :filter_styles, :safelink, :no_pseudo_protocols, :smart).to_html
