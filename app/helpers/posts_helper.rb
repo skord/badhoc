98726e6a99669803611984632722f5d5398e27b5
Mike Danko <mike@l4m3.com>
2011-02-15 10:16:40 -0500

    Revert "fixes bug with pgsql and chart agg"
    This reverts commit e6c2d320a6e43eabbf289f40209c72d2f24986de.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
   def posts_chart_series(posts, start_time)
-    posts_by_day = posts.where(:created_at => start_time.beginning_of_day..Time.zone.now.end_of_day).group("date(posts.created_at)").select("posts.created_at, count(*) as post_count")
+    posts_by_day = posts.where(:created_at => start_time.beginning_of_day..Time.zone.now.end_of_day).group("date(created_at)").select("created_at, count(*) as post_count")
     (start_time.to_date..Date.today).map do |date|
       post = posts_by_day.detect {|post| post.created_at.to_date == date}
       post && post.post_count.to_f || 0
