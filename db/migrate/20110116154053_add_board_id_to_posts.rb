7036c472b39eabf9ff49d995c95e7d494a14031d
Mike Danko <mike@l4m3.com>
2011-02-27 21:34:32 -0500

    closes #19

--- db/migrate/20110116154053_add_board_id_to_posts.rb
+++ db/migrate/20110116154053_add_board_id_to_posts.rb
@@ -2,7 +2,7 @@ class AddBoardIdToPosts < ActiveRecord::Migration
   def self.up
     add_column :posts, :board_id, :integer
     add_index :posts, :board_id
-    Post.update_all(:board_id => Board.first.id)
+    Post.update_all(:board_id => Board.first.id) unless Board.count == 0
   end
 
   def self.down
