80245b6b472c5c87e793118da5051e55ad3d2922
Mike Danko <mike@l4m3.com>
2011-03-17 07:40:23 -0400

    fixed typo in post migration

--- db/migrate/20110316155147_add_image_dimensions_to_post.rb
+++ db/migrate/20110316155147_add_image_dimensions_to_post.rb
@@ -1,7 +1,7 @@
 class AddImageDimensionsToPost < ActiveRecord::Migration
   def self.up
     add_column :posts, :image_width, :integer, :default => 0
-    add_column :posts, :image_height, :interger, :default => 0
+    add_column :posts, :image_height, :integer, :default => 0
   end
 
   def self.down
