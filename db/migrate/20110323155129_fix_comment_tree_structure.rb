8777f7a83337265e3e13261ee856682beed55e3e
Mike Danko <danko@theorypump.com>
2011-03-23 13:08:13 -0400

    removed potentially dangerous part of migration

--- db/migrate/20110323155129_fix_comment_tree_structure.rb
+++ db/migrate/20110323155129_fix_comment_tree_structure.rb
@@ -3,9 +3,6 @@ class FixCommentTreeStructure < ActiveRecord::Migration
     say_with_time "Creating comment tree structure" do
       Comment.rebuild!
     end
-    say_with_time "Setting current comment depth" do
-      Comment.all.each {|comment| comment.move_to_root}
-    end
   end
 
   def self.down
