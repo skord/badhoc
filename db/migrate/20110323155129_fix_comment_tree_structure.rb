ae0f68c38d27d79e49790fbb0caaae5a37645167
Mike Danko <danko@theorypump.com>
2011-03-23 13:04:30 -0400

    fixing migration that works with new nested set gem

--- db/migrate/20110323155129_fix_comment_tree_structure.rb
+++ db/migrate/20110323155129_fix_comment_tree_structure.rb
@@ -1,8 +1,8 @@
 class FixCommentTreeStructure < ActiveRecord::Migration
   def self.up
-#    say_with_time "Creating comment tree structure" do
-#      Comment.rebuild!
-#    end
+    say_with_time "Creating comment tree structure" do
+      Comment.rebuild!
+    end
     say_with_time "Setting current comment depth" do
       Comment.all.each {|comment| comment.move_to_root}
     end
